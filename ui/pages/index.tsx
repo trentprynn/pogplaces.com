78e50a0b9d1c2fc808ab2b519c8d2ce1e43ad7a7
Trent Prynn <trentprynn@gmail.com>
2023-01-10 08:15:14 -0700

    fix: redirect / to /dashboard if logged in

--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -1,6 +1,19 @@
+import useUser from 'data/use-user'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import { Col, Row } from 'react-bootstrap'
 
 export default function Root() {
+    const router = useRouter()
+    const { user, loggedOut } = useUser()
+
+    // if logged in, redirect to the dashboard
+    useEffect(() => {
+        if (!loggedOut) {
+            router.replace('/dashboard')
+        }
+    }, [user, router, loggedOut])
+
     return (
         <Row className="justify-content-center">
             <Col xs="auto">
